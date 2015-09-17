8424b528c1ec32a14e10a84ae1d3000b148fc7fd
shownkwok <SHAWN@kuruwa-kenmatomacbook-pro.local>
2015-09-17 14:09:04 +0800

    Initial Commit

---
+++ TripStuff2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TripStuff2
+//
+//  Created by SHAWN on 9/17/15.
+//  Copyright © 2015 SHAWN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
